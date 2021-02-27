1833a27a542e53969842285b3250807eaf0ca98d
Siubidubi <ebury2433@gmail.com>
2021-02-27 13:22:56 +0100

    Ground
    Ground

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public static int StatycznaZmienna = 1;
-    [SerializeField] private float speed = 12f;
-    [SerializeField] private float gravity = 10f;
-    private Vector3 velocity;
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    private bool isGrounded;
+
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -10f;
+    private Vector3 velocity;
     private CharacterController characterController = null;
 
     private void Awake()
@@ -18,15 +20,58 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        //pobiera dane z klawiatury
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //porusza nasza postacia
+        if (Physics.Raycast(
+           groundCheck.position,
+           transform.TransformDirection(Vector3.down),
+           out RaycastHit hit,
+           0.4f,
+           groundMask))
+        {
+            string terrainType = hit.collider.gameObject.tag;
+
+            switch (terrainType)
+            {
+                default:
+                    speed = 12f;
+                    break;
+                case "Low":
+                    speed = 3f;
+                    break;
+                case "High":
+                    speed = 20f;
+                    break;
+            }
+        }
+     
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * speed * Time.deltaTime);
 
+        //obsluguje nasza grawitacje
+        isGrounded = Physics.CheckSphere(
+            groundCheck.position,
+            0.4f,
+            groundMask
+        );
         velocity.y += gravity * Time.deltaTime;
+
+        //czy spadamy
+        if (!isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        //czy jesteśmy na ziemii
+        else
+        {
+            velocity.y = 0;
+        }
+
         characterController.Move(velocity);
-
     }
-
 }
+
+
